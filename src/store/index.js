b710a7521b913c23a9c772b29d0104d1ccbf2927
Luana Morais <luanamorais.dev@gmail.com>
2021-01-13 15:39:42 -0300

    Config vuex para formulário usuário

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import { api } from "@/services/services.js";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  stric: true,
   state: {
     login: false,
     usuario: {
@@ -14,6 +15,7 @@ export default new Vuex.Store({
       senha: "",
       cep: "",
       rua: "",
+      numero: "",
       bairro: "",
       cidade: "",
       estado: "",
@@ -24,7 +26,7 @@ export default new Vuex.Store({
       state.login = payload;
     },
     UPDATE_USUARIO(state, payload) {
-      state.usuario = payload;
+      state.usuario = Object.assign(state.usuario, payload);
     },
   },
   actions: {
